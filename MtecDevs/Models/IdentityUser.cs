24468aefb80a38a43b5f36ba05297dbced4efdc2
MatheusRisatti <risattimatheus@gmail.com>
2023-09-11 08:48:49 -0300

    inicia o projeto e cria as classes

---
+++ MtecDevs/Models/IdentityUser.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MtecDevs.Models;
+
+    public class IdentityUser
+    {
+        
+    }
